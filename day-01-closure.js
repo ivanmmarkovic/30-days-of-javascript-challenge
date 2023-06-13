f7401fb8b9b126b3598356f19803ff5661f6219c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-14 01:11:33 +0200

    Closure

---
+++ day-01-closure.js
@@ -0,0 +1,21 @@
+/*
+Write a function createHelloWorld. It should return a new function that always returns "Hello World". 
+
+Runtime 57 ms Beats 55.38%
+Memory 41.4 MB Beats 82.83%
+
+*/
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

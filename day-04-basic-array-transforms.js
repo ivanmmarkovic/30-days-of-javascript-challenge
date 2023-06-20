b6acdb0f47c9371f0013ceb33c1534cebb8b6dea
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-21 01:17:38 +0200

    Day 4 basic array transform

---
+++ day-04-basic-array-transforms.js
@@ -0,0 +1,30 @@
+/*
+
+https://leetcode.com/problems/apply-transform-over-each-element-in-array
+
+Runtime 55 ms Beats 76.87%
+Memory 41.6 MB Beats 91.80%
+
+Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+*/
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let r = [];
+
+    for(let i = 0; i < arr.length; i++){
+        r.push(fn(arr[i], i));
+    }
+
+    return r;
+};
